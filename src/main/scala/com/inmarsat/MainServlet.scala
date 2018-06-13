f12554a45c7589c94d66c667baa66aee26b9813c
root <root@jenkins.c.my-project-learning-2018.internal>
2018-06-13 15:13:22 +0000

    Initial Commit

---
+++ src/main/scala/com/inmarsat/MainServlet.scala
@@ -0,0 +1,15 @@
+package com.inmarsat
+
+import org.scalatra.ScalatraServlet
+import org.scalatra.scalate.ScalateSupport
+
+
+class MainServlet extends ScalatraServlet with ScalateSupport {
+  before() {
+    contentType = "text/html"
+  }
+
+  get("/") {
+    layoutTemplate("/WEB-INF/templates/views/index.ssp")
+  }
+}
